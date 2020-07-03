f87051ef24d3bc0da9e574b5cdf3423d34e52604
Lucas de Miranda Lima <60715381+LucasLima939@users.noreply.github.com>
2020-07-03 10:59:08 -0300

    mostrando o funcionamento

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:obaratao/pedidos_widget.dart';
-import 'layout_color.dart';
+import 'package:obaratao/views/lista_pedidos/pedidos_widget.dart';
+import 'widgets/layout_color.dart';
+
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   final routes = <String, WidgetBuilder>{
     PedidosWidget.tag: (BuildContext context) => PedidosWidget(),
   };
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: LayoutColor.secondaryColor,
+        primarySwatch: LayoutColor.primaryColor,
       ),
       home: PedidosWidget(),
       //routes: routes,
---
+++ lib/views/lista_pedidos/pedidos_widget.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import '../../widgets/layoutApp.dart';
+import 'recebePedido.dart';
+
+class PedidosWidget extends StatelessWidget {
+  static String tag = 'pedidos';
+  @override
+  Widget build(BuildContext context) {
+    final content = Pedidos();
+    return LayoutTeste.getLayoutTesteContent(context, content);
+  }
+}
+
+class Pedidos extends StatefulWidget {
+  @override
+  _PedidosState createState() => _PedidosState();
+}
+
+class _PedidosState extends State<Pedidos> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          color: Colors.blue,
+          width: 300,
+          height: 100,
+        )
+      ],
+    );
+  }
+}
